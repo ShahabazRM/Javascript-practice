f60f1c55f159b5c2667a6ca172ee3a1e1cfc74a9
Shahabaz Madihalli <shahabazmadihalli@gmail.com>
2025-08-25 15:13:13 +0000

    datatypes in js

---
+++ 02_Datatypes/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version 
+// alert(3 + 3); // we are using nodejs not browser it doesnt support alert
+
+
+
+let name = "Shahabaz"; //string
+let age = 35; //number
+let isApproved = true; //boolean
+
+
+// data types in java script
+//number => 2 to power 53
+//bigint
+//string => 'a' "a" `a`
+//boolean => true/false
+//null =>  standalone value
+//undefined => 
+//object
+//symbol => unique
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof null);  //type comes as object
+console.log(typeof undefined);  //type comes as undefined
